824f406a5b3b1b85af4510152db4216372c30b93
Aleks Sanchez <alek.sanchez0311@gmail.com>
2026-07-16 09:52:08 -0400

    build: update Kotlin and Compose versions, adjust JVM target to 1.8, and configure compiler exports

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.24" apply false
 }
 
 // Opcional: configuraciones comunes de Gradle pueden colocarse aquí en el futuro.
